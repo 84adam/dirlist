16d4d97f892e80271fd789a0e9d36e14b9e6bf53
84adam <84adam@users.noreply.github.com>
2016-03-21 06:35:15 -0700

    update to add $location argument, log in $HOME
    dirlist <$location (directory name)>
    now appends to a consistent location in $HOME/dir_list_log

--- dirlist.sh
+++ dirlist.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
+# log/append contents of given directory to $HOME/dir_list_log with timestamp.
 
-# List all contents in a directory and write (append) the output to file: dir_list.txt
+# create $location variable for re-use within the program
 
-ls >> dir_list.txt
+location=$1
 
-# Append the date and time to dir_list.txt
+# create/append header message to dir_list_log file in $HOME
+# include location given as argument
+# include time and date of directory content retrieval
 
-echo "... END OF ENTRY FOR `date +\"%d\"\/\"%m\"\/\"%Y\"` AS OF `date +\"%r\"`." >> dir_list.txt
+echo "#### $location <ON> \
+`date +\"%d\"\/\"%m\"\/\"%Y\"` <AT> \
+`date +\"%r\"` ####" \
+>> $HOME/dir_list_log
 
+# list the contents of directory given as input to command 'dirlist'
+# output/append listing to dir_list_log
+
+ls $location >> $HOME/dir_list_log
+
+# create/append 'end of listing' message to dir_list_log
+
+echo ">>>> End of listing for $location. <<<<" >> $HOME/dir_list_log
+echo " " >> $HOME/dir_list_log
+
+# output 'operation complete' message to console
+
+echo "Operation Complete: Logged to $HOME/dir_list_log."
+echo " "
+echo "#### $location <ON> \
+`date +\"%d\"\/\"%m\"\/\"%Y\"` <AT> \
+`date +\"%r\"` ####"
+ls -1 $location
+echo ">>>> End of listing for $location. <<<<"
+echo " "
